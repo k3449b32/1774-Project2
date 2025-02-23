2005642a9b5911b30be67857e4253ed7140074ce
k3449b32 <122392952+k3449b32@users.noreply.github.com>
2025-02-23 12:26:25 -0500

    modified bus class

--- bus.py
+++ bus.py
@@ -5,9 +5,12 @@ class Bus:
 
     counter = 0
 
-    def __init__(self, name: str, base_kv: float):
+    def __init__(self, name: str, base_kv: float, vpu:float,delta: float, bus_type: str):
+        self.bus_type = bus_type
         self.name = name
         self.base_kv = base_kv
+        self.float=float
+        self.delta=delta
         # Keeping an index of all bus instances
         self.index = Bus.counter
         Bus.counter += 1
