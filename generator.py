01be4ba95fd7a4996f28821275a7e64d7b4717cf
Lad198 <155990475+Lad198@users.noreply.github.com>
2025-02-25 08:17:26 -0500

    gen and load

---
+++ generator.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+from bus import Bus
+
+class Generator:
+
+    def __init__(self, name: str, bus: Bus, voltage_setpoint: float, mw_setpoint: float):
+        self.name = name
+        self.bus = bus
+        self.voltage_setpoint = voltage_setpoint
+        self.mw_setpoint = mw_setpoint
+        
---
+++ load.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+from bus import Bus
+
+class Load:
+
+    def __init__(self, name: str, bus: Bus, real_power: float, reactive_power: float):
+        self.name = name
+        self.bus = bus
+        self.real_power = real_power
+        self.reactive_power = reactive_power
