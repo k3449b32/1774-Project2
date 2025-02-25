b9eab46066198fb1ffa7996b4c636f6a6928c856
Lad198 <155990475+Lad198@users.noreply.github.com>
2025-02-25 08:37:55 -0500

    m6 need validated

--- bus.py
+++ bus.py
@@ -4,11 +4,19 @@ import pandas as pd
 class Bus:
 
     counter = 0
+    ACCEPTABLE_BUS_TYPES = ['Slack', 'PQ', 'PV']
 
-    def __init__(self, name: str, base_kv: float, vpu:float,delta: float, bus_type: str):
+    def __init__(self, name: str, base_kv: float, bus_type: str, vpu: float = 1.0, delta: float = 0.0):
+
+        # Ensure bus_type is valid
+        if bus_type not in Bus.ACCEPTABLE_BUS_TYPES:
+            raise ValueError(f"Invalid bus type '{bus_type}'. Acceptable types are {Bus.ACCEPTABLE_BUS_TYPES}.")
+        self.bus_type = bus_type
+
         self.bus_type = bus_type
         self.name = name
         self.base_kv = base_kv
+        self.vpu = vpu
         self.float=float
         self.delta=delta
         # Keeping an index of all bus instances
